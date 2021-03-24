f35baf194b5408c5eae4decb78fdf9ca55d4f883
zanhorst <zanderhorstman@gmail.com>
2021-03-24 11:07:32 -0400

    added listing model

---
+++ app/models/listing.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+  location: {
+    type: String,
+    required: true
+  },
+  dateAvail: {
+    type: Date,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  // only the owner can edit the listing.
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toObject: {
+    // remove `hashedPassword` field when we call `.toObject`
+    transform: (_doc, listing) => {
+      return {
+        id: listing._id,
+        location: listing.location,
+        dateAvail: listing.dateAvail,
+        date: listing.date.toDateString(),
+        price: listing.price,
+        description: listing.description
+      }
+    }
+  }
+})
+
+module.exports = mongoose.model('listing', listingSchema)
